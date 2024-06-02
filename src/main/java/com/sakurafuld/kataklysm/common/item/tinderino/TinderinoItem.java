b1d01f1ecfdcf018271fc9825bf8be52dbc0cbe9
Sakurafuld <168341875+Sakurafuld@users.noreply.github.com>
2024-06-02 15:11:19 +0900

    Delete src/main directory

--- src/main/java/com/sakurafuld/kataklysm/common/item/tinderino/TinderinoItem.java
+++
@@ -1,9 +0,0 @@
-package com.sakurafuld.kataklysm.common.item.tinderino;
-
-import net.minecraft.world.item.Item;
-
-public class TinderinoItem extends Item {
-    public TinderinoItem(Properties prop) {
-        super(prop);
-    }
-}
